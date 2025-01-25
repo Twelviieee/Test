bfb18a893c5da1cb2cc58507df92c426e25dc0d3
Twelviieee <twelvetwtwtwtw@gmail.com>
2025-01-25 22:53:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'Matrix.php';
+
+// Создаем две матрицы
+$matrixA = new Matrix([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]);
+
+$matrixB = new Matrix([
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1],
+]);
+
+// Сложение матриц
+$sumMatrix = $matrixA->add($matrixB);
+echo "Сумма матриц:\n";
+echo $sumMatrix;
+
+// Вычитание матриц
+$differenceMatrix = $matrixA->subtract($matrixB);
+echo "Разность матриц:\n";
+echo $differenceMatrix;
